780819832e4a96da7d43b1771593077c677b9b97
Gustavo <gust040825@gmail.com>
2023-03-29 17:08:12 -0600

    init and finish project

---
+++ models/sockets.js
@@ -0,0 +1,31 @@
+const Marcadores = require('./marcadores')
+
+class Sockets {
+  constructor (io) {
+    this.io = io
+    this.marcadores = new Marcadores()
+    this.socketEvents()
+  }
+
+  socketEvents () {
+    // On connection
+    this.io.on('connection', (socket) => {
+      // TODO: Marcadores menos activos
+      socket.emit('marcadores-activos', this.marcadores.activos)
+
+      // TODO: marcador nuevo
+      socket.on('marcador-nuevo', (marcador) => {
+        this.marcadores.agregarMarcador(marcador)
+        socket.broadcast.emit('marcador-nuevo', marcador)
+      })
+
+      // TODO: Marcador actualizado
+      socket.on('marcador-actualizado', (marcador) => {
+        this.marcadores.actualizarMarcador(marcador)
+        socket.broadcast.emit('marcador-actualizado', marcador)
+      })
+    })
+  }
+}
+
+module.exports = Sockets
